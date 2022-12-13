defc9487c985dd13fd97af4510bad517eec20515
nguyenvanhoai <nguyenvanhoai2411998@gmail.com>
2022-12-13 10:27:52 +0900

    upload Ip adress checker by regex

---
+++ JavaSolution/IpAdressRegex/IpAdress_Regex.java
@@ -0,0 +1,19 @@
+package JavaSolution;
+
+import java.util.Scanner;
+
+public class IpAdress_Regex {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+		in.close();
+	}
+}
+
+class MyRegex {
+	public String pattern = "(\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
+}
